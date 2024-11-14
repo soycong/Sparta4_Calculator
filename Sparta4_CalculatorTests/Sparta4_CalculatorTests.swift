34311ab3fcf6226f49d3dafcc864bd75b5d28e32
soy <soysauce1223@naver.com>
2024-11-14 17:39:39 +0900

    Initial Commit

---
+++ Sparta4_CalculatorTests/Sparta4_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  Sparta4_CalculatorTests.swift
+//  Sparta4_CalculatorTests
+//
+//  Created by seohuibaek on 11/14/24.
+//
+
+import Testing
+@testable import Sparta4_Calculator
+
+struct Sparta4_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

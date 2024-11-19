9ae5640f86705e21404cd712a20071538cc4a557
soy <soysauce1223@naver.com>
2024-11-19 17:28:59 +0900

    [FEAT] Lv.8 '=' 버튼 클릭하면 연산 수행 CalculatorViewController.swift에 calculate, calculateNumbers 함수를 추가하여  calculate를 통해 계산된 식을 calculateNumbers 통해 UI에 출력 되도록 했다.

--- Sparta4_Calculator/CalculatorViewController.swift
+++ Sparta4_Calculator/CalculatorViewController.swift
@@ -23,6 +23,8 @@ class CalculatorViewController: UIViewController {
         calculatorView.numberButtons.forEach { button in
             if button.title(for: .normal) == "AC" { //AC 기능 분리
                 button.addTarget(self, action: #selector(clearLabel), for: .touchUpInside)
+            } else if button.title(for: .normal) == "=" {
+                button.addTarget(self, action: #selector(calculateNumbers), for: .touchUpInside)
             } else {
                 button.addTarget(self, action: #selector(numberButtonTapped(_:)), for: .touchUpInside)
             }
@@ -43,5 +45,30 @@ class CalculatorViewController: UIViewController {
     // AC 버튼으로 초기화
     @objc func clearLabel() {
         calculatorView.numberLabel.text = "0"
+    }
+    
+    // = 버튼 누르면 연산 시작
+    @objc func calculateNumbers() {
+        // 옵셔널 바인딩을 사용하여 nil이 아닌 경우에만 결과를 numberLabel에 할당
+        if let result = calculate(expression: calculatorView.numberLabel.text ?? "Error") {
+            calculatorView.numberLabel.text = String(result)
+        } else {
+            calculatorView.numberLabel.text = "Error"
+        }
+    }
+    
+    /// 수식 문자열을 넣으면 계산해주는 메서드.
+    ///
+    /// 예를 들어 expression 에 "1+2+3" 이 들어오면 6 을 리턴한다.
+    /// 잘못된 형식의 수식을 넣으면 앱이 크래시 난다. ex) "1+2++"
+    func calculate(expression: String) -> Int? {
+        let expression = NSExpression(format: expression)
+        if let result = expression.expressionValue(with: nil, context: nil) as? Int {
+            print(result)
+            return result
+        } else {
+            print("error")
+            return nil
+        }
     }
 }
